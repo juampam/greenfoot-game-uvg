8d1a33bc965848aff8a38c9006455c7b377d138a
juampa <juampa@LAP-JMURALLES.sim.corp>
2023-07-22 18:39:52 -0600

    add background

---
+++ Example/Shepherd_logic.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ *logic game 
+ * 
+ * @author Juan Pablo Muralles, Gustavo Joge
+ * @version 1.0
+ */
+public class Shepherd_logic extends World
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public Shepherd_logic()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+       
+      
+    }
+}
