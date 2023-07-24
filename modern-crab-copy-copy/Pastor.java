48f786d8ff766d2c87b35e8f5619411e348d0262
juampa <juampa@LAP-JMURALLES.sim.corp>
2023-07-23 19:19:20 -0600

    add another project

---
+++ modern-crab-copy-copy/Pastor.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pastor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pastor extends Actor
+{
+    /**
+     * Act - do whatever the Pastor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
